03946ee3f31847e2a3284da3b3dfc2206d109fd1
Francesco Rodriguez <lrodriguezsanc@gmail.com>
2012-02-06 14:39:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Supertodos::Application.config.secret_token = 'b1bea7c1ddb26dbf9b837e497154faaf78745d63ee16100b20ccb26d770b8a0b287e725c39df94a449dc98dd8e53fc330f6c8cd765052f2dd7fda4ee4a4bf95b'
